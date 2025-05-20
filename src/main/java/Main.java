3a615997c8411363576706d1ce611977dd5f200a
Ed Fazylov <efazylov@inno.tech>
2025-05-20 19:19:14 +0300

    Добавлена цикличная проверка пути к файлу с возможностью ввода пути в консоли приложения

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int num1 = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        int num2 = new Scanner(System.in).nextInt();
-        int sum = num1 + num2;
-        int difference = num1 - num2;
-        int product = num1 * num2;
-        double quotient = (double) num1 / num2;
-        System.out.println("Сумма: " + sum);
-        System.out.println("Разница: " + difference);
-        System.out.println("Произведение: " + product);
-        System.out.println("Частное: " + quotient);
+        Scanner scanner = new Scanner(System.in);
+        int requestCounter = 0;
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+            File file = new File(path);
+            boolean fileNotFound = file.exists();
+            boolean isFolder = file.isDirectory();
+            if (!fileNotFound || isFolder) {
+                System.out.println("Введен ошибочный путь - файл не существует/путь приводит к папке");
+                continue;
+            } else {
+                requestCounter++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + requestCounter);
+            }
+        }
     }
+
 }
